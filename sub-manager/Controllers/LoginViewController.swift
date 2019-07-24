20c76f99595b71bd13b5374b93bd2e3157835422
yanesss <aaronc.yanes@calbaptist.edu>
2019-07-23 21:31:31 -0700

    new user registration

--- sub-manager/Controllers/LoginViewController.swift
+++ sub-manager/Controllers/LoginViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     
